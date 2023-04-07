11b105f4bb2e036fe35f51543000fc5cb2a23a99
Dieote <gomezmdiego97@gmail.com>
2023-04-07 18:45:01 +0200

    Continuacion

--- PracticaManejoJDBC/src/main/java/datos/PersonaJDBC.java
+++ PracticaManejoJDBC/src/main/java/datos/PersonaJDBC.java
@@ -1,7 +1,10 @@
 package datos;
 
 import static datos.ConexionPracticas.*;
+import static datos.UsuarioJDBC.SQL_DELETE;
+import static datos.UsuarioJDBC.SQL_INSERT;
 import dominio.Persona;
+import dominio.Usuario;
 import java.sql.*;
 import java.util.*;
 
@@ -11,20 +14,17 @@ public class PersonaJDBC {
     public static final String SQL_INSERT = "INSERT INTO usuario (idPerson, namePerson, yearsPerson, emailPerson) VALUES (?, ?, ?, ?)";
     public static final String SQL_UPDATE = "UPDATE usuario SET namePerson = ?, surnamePerson = ?, yearsPerson = ?, emailPerson = ? WHERE id_User ?";
     public static final String SQL_DELETE = "DELETE FROM usuario WHERE idPerson = ?";
-     
-    public List<Persona> seleccionar (){
+
+    public List<Persona> seleccionar() {
         Connection conn = null;
         PreparedStatement stmt = null;
         ResultSet resul = null;
         Persona person = null;
         List< Persona> persons = new ArrayList<>();
-        
-        try{
+        try {
             conn = getConnection();
             stmt = conn.prepareStatement(SQL_SELECT);
             int idPerson = resul.getInt("IdPerson");
-            
-            
         } catch (SQLException ex) {
             ex.printStackTrace(System.out);
         } finally {
@@ -37,5 +37,82 @@ public class PersonaJDBC {
             }
         }
         return persons;
+    }
+
+    public int inserte(Persona person) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int registroInserts = 0;
+        try {
+            conn = getConnection();
+            stmt = conn.prepareStatement(SQL_INSERT);
+            stmt.setInt(1, person.getIdPerson());
+            stmt.setString(2, person.getNamePerson());
+            stmt.setString(3, person.getSurnamePerson());
+            stmt.setInt(4, person.getYearsPerson());
+            stmt.setString(5, person.getEmailPerson());
+            registroInserts = stmt.executeUpdate();
+            System.out.println("Registros de personas Insertados = " + registroInserts);
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            try {
+                close(stmt);
+                close(conn);
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+            }
+            return registroInserts;
+        }
+    }
+    
+    public int actualizar (Persona person) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int registroAct = 0;
+        try {
+            conn = getConnection();
+            stmt = conn.prepareStatement(SQL_UPDATE);
+            stmt.setString(1, person.getNamePerson());
+            stmt.setString(2, person.getSurnamePerson());
+            stmt.setInt(3, person.getYearsPerson());
+            stmt.setString(4, person.getEmailPerson());
+            stmt.setInt(5, person.getIdPerson());
+            registroAct = stmt.executeUpdate();
+            System.out.println("Registros de personas Actualizadas = " + registroAct);
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            try {
+                close(stmt);
+                close(conn);
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+            }
+            return registroAct;
+        }    
+    }
+    
+    public int eliminar (Persona person){
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int registroDelete = 0;
+        try {
+            conn = getConnection();
+            stmt = conn.prepareStatement(SQL_DELETE);
+            stmt.setInt(1, person.getIdPerson());
+            registroDelete = stmt.executeUpdate();
+            System.out.println("Registros de Personas eliminadas = " + registroDelete);
+        }catch (SQLException ex){
+            ex.printStackTrace(System.out);
+        } finally {
+            try {
+                close(stmt);
+                close(conn);
+            }catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+            }
+        }
+        return registroDelete;
     }
 }
