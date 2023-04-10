23c6d378bdfdd915ba9040410ea307c5613e84b8
Dieote <gomezmdiego97@gmail.com>
2023-04-10 14:49:08 +0200

    Agregamos la conexion transaccional

--- jdbc_PracticaManejoTransacciones/src/main/java/datos/UsuarioJDBC.java
+++ jdbc_PracticaManejoTransacciones/src/main/java/datos/UsuarioJDBC.java
@@ -6,22 +6,31 @@ import java.sql.*;
 import java.util.*;
 
 public class UsuarioJDBC {
+    
+    private Connection conexionTransaccional;
 
     public static final String SQL_SELECT = "SELECT (id_User, userName, userPassword) FROM usuario";
     public static final String SQL_INSERT = "INSERT INTO usuario (userName, userPassword) VALUES (?, ?)";
     public static final String SQL_UPDATE = "UPDATE usuario SET userName = ?, userPassword = ? WHERE id_User ?";
     public static final String SQL_DELETE = "DELETE FROM usuario WHERE id_User = ?";
 
-    public List<Usuario> seleccion() {
+    public UsuarioJDBC (){
+        
+    }
+    
+    public UsuarioJDBC(Connection conexionTransaccional) {
+        this.conexionTransaccional = conexionTransaccional;
+    }
+    
+    public List<Usuario> seleccion() throws SQLException {
 
         Connection conn = null;
         PreparedStatement stmt = null;
         ResultSet res = null;
         Usuario user = null;
         List<Usuario> users = new ArrayList<Usuario>();
-
         try {
-            conn = getConnection();
+            conn = this.conexionTransaccional != null ? this.conexionTransaccional : ConexionPracticas.getConnection();
             stmt = conn.prepareStatement(SQL_SELECT);
             res = stmt.executeQuery();
             while (res.next()) {
@@ -31,8 +40,6 @@ public class UsuarioJDBC {
                 user = new Usuario(id_User, userName, userPassword);
                 users.add(user);
             }
-        } catch (SQLException ex) {
-            ex.printStackTrace(System.out);
         } finally {
             try {
                 close(res);
@@ -45,21 +52,19 @@ public class UsuarioJDBC {
         return users;
     }
 
-    public int inserte(Usuario user) {
+    public int inserte(Usuario user) throws SQLExceptio {
 
         Connection conn = null;
         PreparedStatement stmt = null;
         int registroInserts = 0;
         try {
-            conn = getConnection();
+            conn = this.conexionTransaccional != null ? this.conexionTransaccional : ConexionPracticas.getConnection();
             stmt = conn.prepareStatement(SQL_INSERT);
             stmt.setString(1, user.getUserName());
             stmt.setString(2, user.getUserPassword());
             registroInserts = stmt.executeUpdate();
 
             System.out.println("Registros de Usuarios Insertados = " + registroInserts);
-        } catch (SQLException ex) {
-            ex.printStackTrace(System.out);
         } finally {
             try {
                 close(stmt);
@@ -71,19 +76,17 @@ public class UsuarioJDBC {
         }
     }
 
-    public int actualizar(Usuario user) {
+    public int actualizar(Usuario user) throws SQLExceptio{
         Connection conn = null;
         PreparedStatement stmt = null;
         int registroAct = 0;
         try{
-            conn = getConnection();
+            conn = this.conexionTransaccional != null ? this.conexionTransaccional : ConexionPracticas.getConnection();
             stmt = conn.prepareStatement(SQL_UPDATE);
             stmt.setString(1, user.getUserName());
             stmt.setString(2, user.getUserPassword());
             registroAct = stmt.executeUpdate();
             System.out.println("Registros de Usuarios Actualizados = " + registroAct);
-        } catch (SQLException ex){
-            ex.printStackTrace(System.out);
         } finally {
             try {
                 close(stmt);
@@ -94,19 +97,16 @@ public class UsuarioJDBC {
         }
         return registroAct;
     }
-    public int eliminar (Usuario user){
+    public int eliminar (Usuario user) throws SQLException{
         Connection conn = null;
         PreparedStatement stmt = null;
         int registroDelete = 0;
         try {
-            conn = getConnection();
+            conn = this.conexionTransaccional != null ? this.conexionTransaccional : ConexionPracticas.getConnection();
             stmt = conn.prepareStatement(SQL_DELETE);
             stmt.setInt(1, user.getId_User());
             registroDelete = stmt.executeUpdate();
             System.out.println("Registros de Usuarios eliminados = " + registroDelete);
-            
-        }catch (SQLException ex){
-            ex.printStackTrace(System.out);
         } finally {
             try {
                 close(stmt);
--- jdbc_PracticaManejoTransacciones/src/main/java/test/MainUsuarios.java
+++ jdbc_PracticaManejoTransacciones/src/main/java/test/MainUsuarios.java
@@ -1,32 +1,68 @@
 package test;
 
+import datos.ConexionPracticas;
 import datos.UsuarioJDBC;
 import dominio.Usuario;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.*;
 
 public class MainUsuarios {
 
     public static void main(String[] args) {
-        UsuarioJDBC user = new UsuarioJDBC();
-
-       //Listar Usuarios
-        List<Usuario> usuarios = new ArrayList<>();
-        for (Usuario usuario : usuarios) { 
-        //Insertamos Usuarios
-            usuarios.add(new Usuario("Maru3421", "dasda*21"));
-            usuarios.add(new Usuario("Car123lo", "fera*121"));
-            usuarios.add(new Usuario("anDR0s", "fre21Str"));
-            usuarios.add(new Usuario("Maqa12", "lalalon*234"));
-            usuarios.add(new Usuario("luch099", "kakre1421"));
-            usuarios.add(new Usuario("JOse193", "daND2ll1"));
-            usuarios.add(new Usuario("JOse193", "daND2ll1"));
+        Connection conexion = null;
+        try {
+            conexion = ConexionPracticas.getConnection();
+            if (conexion.getAutoCommit()) {
+                conexion.setAutoCommit(false);
+        }
+        UsuarioJDBC user = new UsuarioJDBC(conexion);
+            Usuario cambioUser = new Usuario();
+            cambioUser.setId_User(1);
+            cambioUser.setUserName("juan.perez.1");
+            cambioUser.setUserPassword("123");
+            user.actualizar(cambioUser);
             
-            //Actualizamos Usuario
-            Usuario userUp = new Usuario(3, "Andiy", "fre21Str");
-            user.actualizar(userUp);
+            Usuario newUser = new Usuario();
+            newUser.setUserName("CarloMans");
+            newUser.setUserPassword("6480ß37");
+            user.inserte(newUser);
             
-             //Eliminar usuario
-            user.eliminar(new Usuario(6));
+            conexion.commit();
+            System.out.println("Se hizo commit");
+            
+        } catch (SQLException ex) {
+        ex.printStackTrace(System.out);
+            System.out.println("Entramos al rollback");
+            try {
+                conexion.rollback();
+            } catch (SQLException ex1) {
+                ex1.printStackTrace(System.out);
+            }
         }
     }
+
+//    public static void main(String[] args) {
+//        UsuarioJDBC user = new UsuarioJDBC();
+//
+//       //Listar Usuarios
+//        List<Usuario> usuarios = new ArrayList<>();
+//        for (Usuario usuario : usuarios) { 
+//        //Insertamos Usuarios
+//            usuarios.add(new Usuario("Maru3421", "dasda*21"));
+//            usuarios.add(new Usuario("Car123lo", "fera*121"));
+//            usuarios.add(new Usuario("anDR0s", "fre21Str"));
+//            usuarios.add(new Usuario("Maqa12", "lalalon*234"));
+//            usuarios.add(new Usuario("luch099", "kakre1421"));
+//            usuarios.add(new Usuario("JOse193", "daND2ll1"));
+//            usuarios.add(new Usuario("JOse193", "daND2ll1"));
+//            
+//            //Actualizamos Usuario
+//            Usuario userUp = new Usuario(3, "Andiy", "fre21Str");
+//            user.actualizar(userUp);
+//            
+//             //Eliminar usuario
+//            user.eliminar(new Usuario(6));
+//        }
+//    }
 }
